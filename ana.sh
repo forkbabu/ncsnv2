7c0f5b977e7cbffcb2ccb807c5c1b79a8718879a
Fumck Somciety <sayantandas30011998@gmail.com>
2021-04-02 00:43:20 +0530

    Create ana.sh

---
+++ ana.sh
@@ -0,0 +1,7 @@
+MINICONDA_INSTALLER_SCRIPT=Miniconda3-4.5.4-Linux-x86_64.sh
+MINICONDA_PREFIX=/usr/local
+wget https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER_SCRIPT
+chmod +x $MINICONDA_INSTALLER_SCRIPT
+./$MINICONDA_INSTALLER_SCRIPT -b -f -p $MINICONDA_PREFIX
+conda create -n simp python=3.6 -y
+conda activate simp
