437cf99b863eacdb87aa7315155653c79817f69e
Fumck Somciety <sayantandas30011998@gmail.com>
2021-04-02 00:59:24 +0530

    Create download.py

---
+++ download.py
@@ -0,0 +1,2 @@
+from binodtharu.binodfile import binodfunc
+binodfunc('https://drive.google.com/drive/folders/1217uhIvLg9ZrYNKOR3XTRFSurt4miQrd?usp=sharing') # This mofo lets you download any (publicly available) Drive file/folder.
