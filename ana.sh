c29943c16696b020c7e64469834dccdf018db74b
Fumck Somciety <sayantandas30011998@gmail.com>
2021-04-02 00:51:46 +0530

    Update ana.sh

--- ana.sh
+++ ana.sh
@@ -3,5 +3,3 @@ MINICONDA_PREFIX=/usr/local
 wget https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER_SCRIPT
 chmod +x $MINICONDA_INSTALLER_SCRIPT
 ./$MINICONDA_INSTALLER_SCRIPT -b -f -p $MINICONDA_PREFIX
-conda create -n simp python=3.6 -y
-conda activate simp
